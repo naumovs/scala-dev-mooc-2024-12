d773387bec39799ecb16a8191319df77f8d9e818
naumovs <s.a.naumov@gmail.com>
2025-01-26 22:22:23 +0300

    ДЗ 1 - Функциональные конструкции языка scala

--- src/main/scala/ru/otus/module1/03-introducing_fp_concepts.scala
+++ src/main/scala/ru/otus/module1/03-introducing_fp_concepts.scala
@@ -1,5 +1,6 @@
 package ru.otus.module1
 
+import scala.::
 import scala.annotation.tailrec
 import scala.language.postfixOps
 
@@ -156,12 +157,15 @@ object hof{
   // Variance
   // 1. Invariance
   // 2. Covariance
-  // 3. Contrvariance
+  // 3. Contravariance
 
   trait Option[+T]{
     def isEmpty: Boolean = if(this.isInstanceOf[None.type]) true else false
 
-    def get: T = ???
+    def get: T = this match {
+      case Some(v) => v
+      case None => throw new NoSuchElementException
+    }
 
     def map[B](f: T => B): Option[B] = flatMap(v => Option(f(v)))
 
@@ -192,12 +196,17 @@ object hof{
    *
    * Реализовать метод printIfAny, который будет печатать значение, если оно есть
    */
+  def printIfAny[T](v: Option[T]): Unit = if (!v.isEmpty) println(v.get)
 
 
   /**
    *
    * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
    */
+  def zip[A, B](o1: Option[A], o2: Option[B]): Option[(A, B)] = (o1, o2) match {
+    case (Some(v1), Some(v2)) => Some((v1, v2))
+    case _ => None
+  }
 
 
   /**
@@ -205,6 +214,7 @@ object hof{
    * Реализовать метод filter, который будет возвращать не пустой Option
    * в случае если исходный не пуст и предикат от значения = true
    */
+  def filter(o: Option[Boolean]): Option[Boolean] = if (!o.isEmpty && o.get) o else None
 
  }
 
@@ -219,7 +229,7 @@ object hof{
 
 
    trait List[+T]{
-     def ::[TT >: T](elem: TT): List[TT] = ???
+     def ::[TT >: T](elem: TT): List[TT] = new ::(elem, this)
    }
    case class ::[T](head: T, tail: List[T]) extends List[T]
    case object Nil extends List[Nothing]
@@ -246,29 +256,67 @@ object hof{
       *
       * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
       */
+    @tailrec
+    def reverse[T](l: List[T], acc: List[T] = Nil): List[T] = l match {
+      case Nil => acc
+      case h :: t => reverse(t, h :: acc)
+    }
 
     /**
       *
       * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
       */
+    def map[T, B](l: List[T], f: T => B): List[B] = {
+      @tailrec
+      def loop(acc: List[B], remaining: List[T]): List[B] = remaining match {
+        case Nil => reverse(acc)
+        case h :: t => loop(f(h) :: acc, t)
+      }
+      loop(Nil, l)
+    }
 
 
     /**
       *
       * Реализовать метод filter для списка который будет фильтровать список по некому условию
       */
+    def filter[T](l: List[T], f: T => Boolean): List[T] = {
+      @tailrec
+      def loop(acc: List[T], remaining: List[T]): List[T] = remaining match {
+        case Nil => reverse(acc)
+        case h :: t => if (f(h)) loop(h :: acc, t) else loop(acc, t)
+      }
+      loop(Nil, l)
+    }
+
+
 
     /**
       *
       * Написать функцию incList котрая будет принимать список Int и возвращать список,
       * где каждый элемент будет увеличен на 1
       */
-
+    def incList(l: List[Int]): List[Int] = {
+      @tailrec
+      def loop(acc: List[Int], remaining: List[Int]): List[Int] = remaining match {
+        case Nil => reverse(acc)
+        case h :: t => loop(h + 1 :: acc, t)
+      }
+      loop(Nil, l)
+    }
 
     /**
       *
       * Написать функцию shoutString котрая будет принимать список String и возвращать список,
       * где к каждому элементу будет добавлен префикс в виде '!'
       */
+    def shoutString(l: List[String]): List[String] = {
+      @tailrec
+      def loop(acc: List[String], remaining: List[String]): List[String] = remaining match {
+        case Nil => reverse(acc)
+        case h :: t => loop("!" + h :: acc, t)
+      }
+      loop(Nil, l)
+    }
 
  }
