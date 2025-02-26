2e93402c03caa21756e591a61d5b65259aac3114
avoronets <avoronets@naumen.ru>
2025-02-26 22:03:15 +0300

    webinar 16 homework

--- src/main/scala/ru/otus/module3/ZIOMain.scala
+++ src/main/scala/ru/otus/module3/ZIOMain.scala
@@ -1,12 +1,20 @@
 package ru.otus.module3
 
+import zio.{Scope, Task, ZIO, ZIOAppArgs, ZIOAppDefault}
+
 
 object ZIOMain {
 
   def main(args: Array[String]): Unit = {
-
+    val z1: Task[Unit] = ZIO.attempt(println("Hello"))
+    zio.Runtime.default.run(z1)
   }
 
+}
+
+object ZIOMain2 extends ZIOAppDefault{
+  override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] =
+    ZIO.attempt(println("Hello"))
 }
 
 
