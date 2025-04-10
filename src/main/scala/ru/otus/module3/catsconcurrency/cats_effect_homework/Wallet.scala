9508af5f92096cf89d6e79bf771ab660d45adf6d
naumovs <s.a.naumov@gmail.com>
2025-04-10 22:50:02 +0300

    ДЗ 9 - cats effect

--- src/main/scala/ru/otus/module3/catsconcurrency/cats_effect_homework/Wallet.scala
+++ src/main/scala/ru/otus/module3/catsconcurrency/cats_effect_homework/Wallet.scala
@@ -1,8 +1,12 @@
-/*package ru.otus.module3.catsconcurrency.cats_effect_homework
+package ru.otus.module3.catsconcurrency.cats_effect_homework
 
-import cats.effect.Sync
+import cats.effect.{IO, Sync}
 import cats.implicits._
-import Wallet._
+import Wallet.{WalletError, _}
+import cats.data.EitherT
+import cats.{Functor, Group}
+import cats.effect.kernel.Resource.{Pure, both}
+import cats.kernel.Semigroup
 
 // DSL управления электронным кошельком
 trait Wallet[F[_]] {
@@ -25,9 +29,38 @@ trait Wallet[F[_]] {
 // - java.nio.file.Files.exists
 // - java.nio.file.Paths.get
 final class FileWallet[F[_]: Sync](id: WalletId) extends Wallet[F] {
-  def balance: F[BigDecimal] = ???
-  def topup(amount: BigDecimal): F[Unit] = ???
-  def withdraw(amount: BigDecimal): F[Either[WalletError, Unit]] = ???
+
+  import java.nio.file._
+
+  private val tmpDir = scala.util.Properties.envOrElse("TMP", "/tmp")
+
+  override def balance: F[BigDecimal] = Sync[F].delay {
+    if (Files.exists(Paths.get(s"$tmpDir/$id"))) {
+      BigDecimal(
+        Files.readString(Paths.get(s"$tmpDir/$id"))
+      )
+    } else {
+      0
+    }
+  }
+  override def topup(amount: BigDecimal): F[Unit] = for {
+    currentBalance <- balance
+    _ <- Sync[F].delay {
+      if (Files.exists(Paths.get(s"$tmpDir/$id"))) {
+        Files.writeString(Paths.get(s"$tmpDir/$id"), Semigroup[BigDecimal]
+          .combine(currentBalance, amount).toString)
+      } else {
+        Files.writeString(Paths.get(s"$tmpDir/$id"), amount.toString,
+          StandardOpenOption.CREATE)
+      }
+    }
+  } yield ()
+
+  override def withdraw(amount: BigDecimal): F[Either[WalletError, Unit]] = for {
+    currentBalance <- balance
+    approved = currentBalance >= amount
+    _ <- Sync[F].whenA(approved)(topup(-amount))
+  } yield if (approved) Right(()) else Left(BalanceTooLow)
 }
 
 object Wallet {
@@ -37,11 +70,14 @@ object Wallet {
   // Здесь нужно использовать обобщенную версию уже пройденного вами метода IO.delay,
   // вызывается она так: Sync[F].delay(...)
   // Тайпкласс Sync из cats-effect описывает возможность заворачивания сайд-эффектов
-  def fileWallet[F[_]: Sync](id: WalletId): F[Wallet[F]] = ???
+  def fileWallet[F[_]: Sync](id: WalletId): F[Wallet[F]] = {
+    for {
+      wallet <- Sync[F].delay(new FileWallet[F](id))
+    } yield wallet
+  }
 
   type WalletId = String
 
   sealed trait WalletError
   case object BalanceTooLow extends WalletError
 }
-*/
